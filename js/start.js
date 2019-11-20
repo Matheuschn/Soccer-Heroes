67c75db4c4718e7047f2cb69ca22e182655579af
Matheus Schneider Camilo <49127810+Matheuschn@users.noreply.github.com>
2019-11-20 14:57:45 -0300

    Adicionado os modos de jogo e eliminadas as variáveis globais. (#3)

--- js/start.js
+++ js/start.js
@@ -1,9 +1,8 @@
 // Importa a próxima cena e a variável game
-import { mainScene } from "./main.js";
-import { game } from "./index.js";
+import { menuScene } from "./menu.js";
 
 // Cria a cena de início
-const startScene = new Phaser.Scene("StartScene");
+const startScene = new Phaser.Scene("startScene");
 
 startScene.preload = function() {
   this.load.image("start", "assets/start.png");
@@ -11,10 +10,10 @@ startScene.preload = function() {
 
 startScene.create = function() {
   // Adiciona uma imagem e espera o clique do usuário
-  let halfWidth = game.scale.width / 2;
-  let halfHeight = game.scale.height / 2;
+  let halfWidth = this.scale.width / 2;
+  let halfHeight = this.scale.height / 2;
   let startButton = this.add.image(halfWidth, halfHeight, "start").setInteractive();
-  startButton.on("pointerdown", () => this.scene.start(mainScene));
+  startButton.on("pointerdown", () => this.scene.start(menuScene));
 };
 
 // Exporta a cena
